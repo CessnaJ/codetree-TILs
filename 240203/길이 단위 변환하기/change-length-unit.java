69a5e354eebcf9770b3eb7f497f031bcbd623d9b
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-03 21:05:24 +0900

    Title: 길이 단위 변환하기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240203/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        float ft = 30.48f;
+        int mi = 160934;
+
+        System.out.printf("9.2ft = %.1fcm", 9.2f * ft);
+        System.out.print("\n");
+        System.out.printf("1.3mi = %.1fcm", 1.3f * mi);
+    }
+}
