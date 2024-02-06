fa3e01eafe47be51255131e7c32f1eafc6b17947
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-06 23:34:04 +0900

    Title: 차례로 출력, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240206/차례로 출력/print-in-order.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int start = 5;
+        int end = 17;
+        for(int i = start; i <= end; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
