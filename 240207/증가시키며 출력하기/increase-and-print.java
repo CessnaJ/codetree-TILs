29efd5da753ba03ee0877ad156edc6177b01d700
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-07 00:24:52 +0900

    Title: 증가시키며 출력하기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240207/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int start = 5;
+        int end = 17;
+
+        for (int n = start; n <= end; n += 2) {
+            System.out.printf("%d ", n);
+        }
+    }
+}
