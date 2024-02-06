524eacd5e648f984ea58b1b220e565cd1fa5f81b
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-06 22:27:28 +0900

    Title: 일 수 구하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/일 수 구하기/number-of-days-in-month.java
+++ 240206/일 수 구하기/number-of-days-in-month.java
@@ -5,9 +5,11 @@ public class Main {
 
         int a = sc.nextInt();
 
-        if(a == 1 ||a == 2 || a == 3 || a == 5 || a == 7 || a == 8 || a == 10 || a == 12){ // 쓰레기문제
+        if(a == 1 || a == 3 || a == 5 || a == 7 || a == 8 || a == 10 || a == 12){ // 쓰레기문제
             System.out.println("31");
-        }else{
+        }else if (a == 2){
+            System.out.println("28");
+        } else {
             System.out.println("30");
         }
     }
