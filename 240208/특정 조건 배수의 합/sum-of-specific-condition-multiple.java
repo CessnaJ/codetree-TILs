9c9cfb3e0f5302b7412e93a111640e4c020d6644
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-08 02:24:44 +0900

    Title: 특정 조건 배수의 합, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
+++ 240208/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -7,9 +7,17 @@ public class Main {
         int b = sc.nextInt();
         int sum = 0;
 
-        for (int i = a; i <= b; i++) {
-            if (i % 5 == 0) {
-                sum += i;
+        if ( a < b) {
+            for (int i = a; i <= b; i++) {
+                if (i % 5 == 0) {
+                    sum += i;
+                }
+            }
+        } else if ( a > b) {
+            for (int i = b; i <= a; i++) {
+                if (i % 5 == 0) {
+                    sum += i;
+                }
             }
         }
         System.out.printf("%d", sum);
