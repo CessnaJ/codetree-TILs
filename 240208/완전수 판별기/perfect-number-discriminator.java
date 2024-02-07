8ade4979e9f148da3b116f71ace88d726f71cdec
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-08 02:40:08 +0900

    Title: 완전수 판별기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/완전수 판별기/perfect-number-discriminator.java
+++ 240208/완전수 판별기/perfect-number-discriminator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 0;
+        int n = sc.nextInt();
         int sum = 0;
 
         for (int i = 1; i < n; i++) {
