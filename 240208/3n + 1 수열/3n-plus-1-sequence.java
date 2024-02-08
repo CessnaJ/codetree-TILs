464c9ab9533986a7ba01ff2ded37f82b0aa2fa6f
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-08 19:36:49 +0900

    Title: 3n + 1 수열, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/3n + 1 수열/3n-plus-1-sequence.java
+++ 240208/3n + 1 수열/3n-plus-1-sequence.java
@@ -6,15 +6,14 @@ public class Main {
         int n = sc.nextInt();
         int cnt = 0;
         while (true) {
+            if (n == 1) {
+                break;
+            }
             cnt++;
             if (n % 2 == 0) {
                 n /= 2;
             } else {
                 n = n * 3 + 1;
-            }
-            
-            if (n == 1) {
-                break;
             }
         }
         System.out.print(cnt);
