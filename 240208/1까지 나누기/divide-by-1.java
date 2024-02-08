5c8bee3da3b0d9b6b6957875dad56ba1b6084e22
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-08 18:46:12 +0900

    Title: 1까지 나누기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/1까지 나누기/divide-by-1.java
+++ 240208/1까지 나누기/divide-by-1.java
@@ -4,18 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int prod = 1;
-        int cnt = 0;
-
         int i = 1;
-        while (true) {
-            prod *= i;
-            cnt++;
-            i++;
-            if (prod > n) {
-                break;
-            }
-        }
-        System.out.println(cnt);
+        int cnt = 0;
+        int div;
+
+        do {
+        n /= i;
+        i++;
+        cnt++;
+        } while (n > 1);
+        System.out.print(cnt);
     }
 }
