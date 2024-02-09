8af467f292da96eecc6dd37f801d560c7821b22e
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-09 22:43:24 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 173ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240209/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 3
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n-i; j > 0; j--) {
+                System.out.print("  ");
+            }
+            for (int k = i; k > 0; k--) {
+                System.out.print("@ ");
+            }
+            System.out.println();
+        }
+        for (int i = n-1; i > 0; i--) {
+            for (int k = i; k > 0; k--) {
+                System.out.print("@ ");
+            }
+            for (int j = n-i; j > 0; j--) {
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
