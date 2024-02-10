26582fc6299e2a7227318fcfd00b6c472a941800
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-11 01:31:32 +0900

    Title: 완전수, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240211/완전수/perfect-number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int cnt = 0;
+        int temp;
+        for (int i = start; i <= end; i++) {
+            temp = 0;
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    temp += j;
+                }
+            }
+            if (temp == i) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
