0824ff2780363e4152a7d101b9ecb7e395a2dd5a
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-13 00:11:19 +0900

    Title: 100 도달하기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240213/100 도달하기/reach-100.java
+++ 240213/100 도달하기/reach-100.java
@@ -7,7 +7,7 @@ public class Main {
         arr[0] = 1;
         arr[1] = sc.nextInt();
         System.out.printf("%d %d ", arr[0], arr[1]);
-        for (int i = 2; i < 10; i++) {
+        for (int i = 2; i < 100; i++) {
             arr[i] = arr[i-1] + arr[i-2];
             System.out.printf("%d ", arr[i]);
             if (arr[i] > 100) {
