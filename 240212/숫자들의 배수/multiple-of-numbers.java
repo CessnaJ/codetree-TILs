89f4fddea575a3b1c050aecfcf4db98b10dba833
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-12 23:49:05 +0900

    Title: 숫자들의 배수, Time: 170ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240212/숫자들의 배수/multiple-of-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[11];
+
+        int cnt = 0;
+        for (int i = 1; i <= 10; i++) {
+            arr[i] = n * i;
+            System.out.print(arr[i] + " ");
+            if (arr[i] % 5 == 0) {
+                cnt++;
+            }
+            if (cnt == 2) {
+                break;
+            }
+        }
+    }
+}
