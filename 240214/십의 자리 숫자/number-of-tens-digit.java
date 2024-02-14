590ba6064d353a15b369990c7cf67e2283042b44
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-14 22:17:06 +0900

    Title: 십의 자리 숫자, Time: 174ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240214/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        for (int i = 0; i < 100; i++) {
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            arr[n/10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("%d - %d\n", i, arr[i]);
+        }
+    }
+}
