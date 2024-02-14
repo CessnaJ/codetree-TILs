402fee00abb892e1d18c15c7c9ad225bbb066f97
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-15 02:06:03 +0900

    Title: 배열 놀이, Time: 207ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240215/배열 놀이/play-with-array.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            arr[i] = num;
+        }
+
+        for (int j = 0; j < q; j++) {
+            int operation = sc.nextInt();
+            
+            if (operation == 1) {
+                int targetA = sc.nextInt();
+                System.out.println(arr[targetA - 1]);
+            } else if (operation == 2) {
+                int targetA = sc.nextInt();
+                int index = 0;
+                for (int k = 0; k < n; k++) {
+                    if (arr[k] == targetA) {
+                        index = k + 1;
+                    }
+                }
+                System.out.println(index);
+                
+            } else if (operation == 3) {
+                int targetA = sc.nextInt();
+                int targetB = sc.nextInt();
+                for (int k = targetA; k <= targetB; k++) {
+                    System.out.printf("%d ", arr[k-1]);
+                }
+            }
+        }
+    }
+}
