b968c9804edaa224364863e26f3fd58b12ad6ade
CessnaJ <109272333+CessnaJ@users.noreply.github.com>
2024-02-17 18:00:52 +0900

    Title: 대각선으로 숫자 채우기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240217/대각선으로 숫자 채우기/diagonal-numbering.java
+++ 240217/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -26,7 +26,7 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 System.out.printf("%d ", arr[i][j]);
             }
             System.out.println();
